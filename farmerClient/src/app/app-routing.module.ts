b7fb7cb80b9d95359c4a6175eb1cfd9329ae8883
Ingeborg Slegers <ingeborgfreda@gmail.com>
2019-03-21 09:41:58 -0400

    fixed routing for markets and auth

--- farmerClient/src/app/app-routing.module.ts
+++ farmerClient/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
 import { SignupComponent } from './signup/signup.component';
 import { AccountComponent } from './account/account.component';
+import { MarketComponent } from './market/market.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "/home", pathMatch: "full" },
-  { path: "home", component: HomeComponent },
+  { path: "", redirectTo: "/markets", pathMatch: "full" },
+  // { path: "home", component: HomeComponent },
+  { path: "markets", component: MarketComponent},
   { path: "auth", component: AuthComponent },
   { path: "account", component: AccountComponent },
   { path: "signup", component: SignupComponent }
